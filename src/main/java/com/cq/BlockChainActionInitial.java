452164b725f6c1bc2e6cade4948872fa5f6675bb
linsilence <linlinsilence@gmail.com>
2021-01-05 13:07:05 +0800

    Update BlockChainActionInitial.java

--- src/main/java/com/cq/BlockChainActionInitial.java
+++ src/main/java/com/cq/BlockChainActionInitial.java
@@ -45,32 +45,25 @@ public class BlockChainActionInitial implements ApplicationRunner {
 
         List<String> params = args.getOptionValues("params");
 
-        Future future = null;
         if (actionName.equals(Eth_CreateAddress.class.getSimpleName())) {
-            future = scheduler.submit(new Eth_CreateAddress(ethWalletService));
+            scheduler.submit(new Eth_CreateAddress(ethWalletService));
         } else if (actionName.equals(Eth_AnalyzeBalance.class.getSimpleName())) {
-            future = scheduler.submit(new Eth_AnalyzeBalance(ethWalletService));
+            scheduler.submit(new Eth_AnalyzeBalance(ethWalletService));
         } else if (actionName.equals(Eth_Transfer.class.getSimpleName())) {
             if (CollectionUtil.isEmpty(params)) {
                 throw new RuntimeException("Eth_Transfer params is empty. --name=Eth_Transfer --params=\"[from, to, amount, isContract]\" (0 - eth, 1 - contract)");
             }
             JSONArray ja = JSONUtil.parseArray(params.get(0));
-            future = scheduler.submit(new Eth_Transfer(ethWalletService, ja.toArray()));
+            scheduler.submit(new Eth_Transfer(ethWalletService, ja.toArray()));
         } else if (actionName.equals(Eth_CollectMain.class.getSimpleName())) {
             if (CollectionUtil.isEmpty(params)) {
                 throw new RuntimeException("Eth_CollectMain params is empty. 0 - eth, 1 - token");
             }
             Integer type = Integer.parseInt(params.get(0));
-            future = scheduler.submit(new Eth_CollectMain(ethWalletService, type));
+            scheduler.submit(new Eth_CollectMain(ethWalletService, type));
         } else if (actionName.equals(Eth_Obfuscate.class.getSimpleName())) {
-            future = scheduler.submit(new Eth_Obfuscate(ethWalletService));
+            scheduler.submit(new Eth_Obfuscate(ethWalletService));
         }
 
-        while (true) {
-            Thread.sleep(1000);
-            if (future != null && future.isDone()) {
-                break;
-            }
-        }
     }
 }
